a19e17368b1c1871d545011cd8dc09c74780255f
Nikitosiwe <nikitos673@gmail.com>
2017-09-20 21:03:32 +0300

    реализован первый вариант поиска слов в тексте.

--- TextAnalysis/Main.py
+++ TextAnalysis/Main.py
@@ -2,6 +2,7 @@ import sys
 
 from PyQt5 import uic
 from PyQt5.QtWidgets import QApplication, QFileDialog
+from PyQt5.QtGui import QPalette, QColor, QTextCursor
 
 from PYModules.AuxiliaryMethods import *
 
@@ -11,21 +12,56 @@ app = QApplication(sys.argv)
 MainWindow = uic.loadUi('UIFiles/MainWindow.ui')
 
 
+MainWindow.plainTextEdit.setPlainText('s dfh akjsdhfkajs dhfka sdkjfh aslkjdf hajsdhfkjas asjdkf askd fhak')
+
+
 #Методы нажатия соответствующих кнопок
-def BrowseButton_click():
+def browseButton_click():
     pass
 
 def searchButton_click():
-    if not checkAnalysisText(MainWindow.plainTextEdit.toPlainText()):
+    analysisText = MainWindow.plainTextEdit.toPlainText()
+    searchText = MainWindow.searchLineEdit.text()
+    if not checkAnalysisText(analysisText):
         return
+    if not checkAnalysisText(searchText):
+        return
+
+    searchWords = getWordsFromString(searchText)
+    if len(searchWords)<1: return
+
+    words = getWordsFromString(analysisText)
+
+    print(searchWord(searchWords[0], analysisText))
+
+    #result = list(set(worlds) & set(searchWorlds))
+    #print(result)
+    #print(getWorldsFromString(analysisText))
+
+
 
     print('Text')
 
 
 #Подключение методов к кнопкам
-MainWindow.BrowseButton.clicked.connect(BrowseButton_click)
+MainWindow.BrowseButton.clicked.connect(browseButton_click)
 MainWindow.searchButton.clicked.connect(searchButton_click)
 
+"""
+p = MainWindow.plainTextEdit.palette()
+p.setColor(QPalette.Highlight, QColor('Red'))
+MainWindow.plainTextEdit.setPalette(p)
+
+c = MainWindow.plainTextEdit.textCursor()
+c.setPosition(5);
+c.setPosition(10, QTextCursor.KeepAnchor)
+MainWindow.plainTextEdit.setTextCursor(c);
+
+c = MainWindow.plainTextEdit.textCursor()
+c.setPosition(30);
+c.setPosition(40, QTextCursor.KeepAnchor)
+MainWindow.plainTextEdit.setTextCursor(c);
+"""
 
 
 #Показ главного окна и старт приложения.
--- TextAnalysis/PYModules/AuxiliaryMethods.py
+++ TextAnalysis/PYModules/AuxiliaryMethods.py
@@ -11,3 +11,24 @@ def checkAnalysisText(text):
             return False
     else:
         print("checkAnalysisText - Это не текст")
+
+
+#Метод разбивает строку на слова и возвращает список найденых слов
+def getWordsFromString(text):
+    #result = re.split('[\.|,|-|\s|\(|\)|\{|\}|\]|\]|:|;|\<|\>|\'|\"]+', text)
+    result = [x for x in re.split('[\W]+', text) if not re.match('\d+',x)]
+    return result
+
+
+#Метод ищет вхождения слова в строке. return ('Cлово',[позиция в строке 1, позиция в строке 2, ....])
+def searchWord(word, s):
+    i=0
+    lst = []
+    while(i!=-1):
+        i = s.find(word, i+1)
+        if i != -1:
+            lst.append(i)
+        else:
+            continue
+
+    return (word, lst)
