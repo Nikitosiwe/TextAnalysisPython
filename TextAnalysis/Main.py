e9744db666953a2196752750ecd183656af42002
Nikitosiwe <nikitos673@gmail.com>
2017-09-22 22:26:25 +0300

    Добавлено событие на нажате кнопки анализа текста

--- TextAnalysis/Main.py
+++ TextAnalysis/Main.py
@@ -2,7 +2,7 @@ import sys
 
 from PyQt5 import uic
 from PyQt5.QtWidgets import QApplication, QFileDialog
-from PyQt5.QtGui import QPalette, QColor, QTextCursor
+from PyQt5.QtGui import QPalette, QColor, QTextCursor, QStandardItemModel, QStandardItem
 
 from PYModules.AuxiliaryMethods import *
 
@@ -10,6 +10,7 @@ from PYModules.AuxiliaryMethods import *
 app = QApplication(sys.argv)
 
 MainWindow = uic.loadUi('UIFiles/MainWindow.ui')
+TextAnalysisWindow = uic.loadUi('UIFiles/TextAnalysisWindow.ui')
 
 
 MainWindow.plainTextEdit.setPlainText('s dfh akjsdhfkajs dhfka sdkjfh aslkjdf hajsdhfkjas asjdkf askd fhak')
@@ -34,13 +35,39 @@ def searchButton_click():
     #Все слова текста
     words = getWordsFromString(analysisText)
 
+
     #Найденные слова
     detectedWords = {k:words[k] for k in searchWords.keys() if k in words.keys() }
+
+def textAnalysisButton_click():
+    print(analysisText(MainWindow.plainTextEdit.toPlainText()))
+
+    print('kykyk')
+    TextAnalysisWindow.show()
+    view = TextAnalysisWindow.columnView
+
+    model = QStandardItemModel()
+
+    try:
+        for groupnum in range(3):
+            group = QStandardItem("Group %s" %(groupnum))
+
+            for personnum in range(5):
+                child = QStandardItem("Person %s (group %s)"% (personnum, groupnum))
+
+                group.appendRow(child)
+
+            model.appendRow(group);
+
+        view.setModel(model)
+    except Exception as ex:
+        print(ex)
 
 
 #Подключение методов к кнопкам
 MainWindow.BrowseButton.clicked.connect(browseButton_click)
 MainWindow.searchButton.clicked.connect(searchButton_click)
+MainWindow.textAnalysisButton.clicked.connect(textAnalysisButton_click)
 
 """
 p = MainWindow.plainTextEdit.palette()
